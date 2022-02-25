817756f1596640bc24e8419bdf0848b113e11d57
Klara Fall <anna.majewski@warad.org>
2022-02-25 11:39:18 +0100

    Remove outcommented code

--- algorithms/manhattan_tourist_problem.py
+++ algorithms/manhattan_tourist_problem.py
@@ -14,20 +14,13 @@ if len(sys.argv) > 1:
         s[0][0] = 0
         for i in range(1, n):
             s[i][0] = s[i - 1][0] + wd[i - 1][0]
-            # print("Durchgang", i, "Ergebnis s[",i, "][0]", s[i][0], "wd = ", wd[i-1][0])
         for j in range(1, m):
             s[0][j] = s[0][j - 1] + wr[0][j - 1]
-            # print("Durchgang", j, "Ergebnis s[0][",j, "]", s[0][j], "wr = ", wr[0][j-1])
         for i in range(1, n):
             for j in range(1, m):
                 a = s[i - 1][j] + wd[i - 1][j]
-                # print("A ist ", a, "wd ist ", wd[i-1][j], "s ist ", s[i-1][j])
                 b = s[i][j - 1] + wr[i][j - 1]
-                # print("B ist ", b, "wr ist ", wr[i][j-1], "s ist ", s[i-1][j-2])
                 s[i][j] = max(a, b)
-                # print("Das Maximum von A und B ist ", s[i][j])
-                ## if a == b:
-                # speicher mir beides
         return s[n - 1][m - 1]
 
 
